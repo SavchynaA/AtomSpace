adeef9b3c6f3214be0fc550c6fb4848d5281b201
Анастасия <75798605+SavchynaA@users.noreply.github.com>
2021-01-14 18:12:01 +0200

    Add files via upload

---
+++ homework2.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{   // условия и циклы
+    setlocale(0, "");
+    
+    const int a = 10;
+    int arr[a];
+    for (int i = 0; i < a; i++) {
+        arr[i] = rand() % 10;
+        printf("%d\n", arr[i]);
+    }
+    std:: cout << "Вывод без 0:" << std::endl;
+    for (int i = 0; i < a; i++) {
+       if (arr[i] == 0) 
+       continue;
+       else  printf("%d\n", arr[i]);
+    }
+    // goto 
+    int n;
+step:
+    printf("Введите число больше 5\n");
+    scanf_s("%d", &n);
+    if (n<=5) goto step;
+    printf("hello world!");  
+}
+
