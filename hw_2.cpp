3914295e7c18b6fa1826f115b396bc9ef8cd1e95
Анастасия <75798605+SavchynaA@users.noreply.github.com>
2021-01-21 17:12:48 +0200

    Add files via upload

---
+++ hw_2.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+int main()
+{
+    setlocale(0, "");
+    char str[100] = "hello";
+    char str2[] = " worldddddddd";
+
+    strncat_s(str, str2, 6);
+    std::cout << str << std::endl;
+
+    char str3[] = "!";
+    strcat_s(str, str3);
+    std::cout << str << std::endl;
+    std::cout << "------------------" << std::endl;
+    printf("Если 1 и 2 строка равны = получим 0, если нет - 1\n");
+    std::cout << strcmp(str, str2) << std::endl;
+
+}
+
+
